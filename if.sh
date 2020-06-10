384e527116aec0c0d590d25eb61e79adf1fab819
rvemulapati <vemulapatirohini@gmail.com>
2020-06-09 21:23:01 -0500

    if

--- if.sh
+++ if.sh
@@ -2,6 +2,8 @@ int1=$1
 int2=$2
 if [ $int1 -eq $int2 ]; then
 echo "values are equal"
+elif [ $int1 -gt $int2 ]; then
+echo "values are greater"
 else
-echo "values are not equal"
+echo "values are not valid"
 fi 
